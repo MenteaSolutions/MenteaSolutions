0069aa5ffb13b7ca1d0fb82aacc3af4bb2bf6f84
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-10-11 07:52:17 +0200

    chore: Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,21 @@ import { AuthService } from './Services/auth.service'; // Correct the path to se
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
+import { UpdatesNotificationComponent } from "./app-updates-notification";
+import '@khmyznikov/pwa-install';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, CommonModule, IonicModule, RouterModule], // Import RouterModule
+  imports: [FormsModule, CommonModule, IonicModule, RouterModule, UpdatesNotificationComponent], // Import RouterModule
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA] // Add CUSTOM_ELEMENTS_SCHEMA to handle ion-* elements
 })
 export class AppComponent {
-  constructor(private menu: MenuController, public authService: AuthService) {}
+  constructor(private menu: MenuController, public authService: AuthService) {
+
+  }
 
   // Method to close the menu
   closeMenu() {
