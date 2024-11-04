3681d666a4499aa3d0aec36dde6e2c699160d069
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-11-04 11:56:38 +0100

    chore: Update Firebase configuration with new project credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
  firebaseConfig : {
-  apiKey: "AIzaSyBfNfUwhSyHJJNmkCCTNMh1jaSDXY24Ye8",
-  authDomain: "zoomnomades-e95cd.firebaseapp.com",
-  databaseURL: "https://zoomnomades-e95cd-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "zoomnomades-e95cd",
-  storageBucket: "zoomnomades-e95cd.firebasestorage.app",
-  messagingSenderId: "200227817044",
-  appId: "1:200227817044:web:f017d74a9cc3928a2acfc1"
-  }
+  apiKey: "AIzaSyBYR7x3S8-qp5iyfK3swOIkCxybcZI5V-Q",
+  authDomain: "tstdeploy-9f481.firebaseapp.com",
+  databaseURL: "https://tstdeploy-9f481-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tstdeploy-9f481",
+  storageBucket: "tstdeploy-9f481.firebasestorage.app",
+  messagingSenderId: "680107044888",
+  appId: "1:680107044888:web:60b03b66b1d0add1ef536d"
+}
 };
